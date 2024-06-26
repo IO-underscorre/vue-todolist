2fe0da40236e09c649cd6dc4fbca88e08c5dba35
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-26 22:40:50 +0200

    refactor: page now work with an array of object as specifications state

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,45 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            completedList: [],
-            todoList: [],
+            todoAndCompletedList: [],
         }
     },
+
+    methods: {
+        changeStatusOfTask(taskToComplete , complete = true) {
+            this.todoAndCompletedList[taskToComplete.indexOfTaskInOriginalArray].done = complete ? true : false;
+        },
+
+        deleteTask(taskToDelete) {
+            this.todoAndCompletedList.splice(taskToDelete.indexOfTaskInOriginalArray , 1);
+        }
+    },
+
+    computed: {
+        getTodoList() {
+            return this.todoAndCompletedList.reduce((newList , task , indexOfTaskInOriginalArray) => {
+                if(!task.done) {
+                    const todoTask = {
+                        task: task.text,
+                        indexOfTaskInOriginalArray: indexOfTaskInOriginalArray
+                    };
+                    newList.push(todoTask);
+                }
+                return newList;
+            } , []);
+        },
+
+        getCompletedList() {
+            return this.todoAndCompletedList.reduce((newList , task , indexOfTaskInOriginalArray) => {
+                if(task.done) {
+                    const completedTask = {
+                        task: task.text,
+                        indexOfTaskInOriginalArray: indexOfTaskInOriginalArray
+                    };
+                    newList.push(completedTask);
+                }
+                return newList;
+            } , []);
+        }
+    }
 }).mount('#app');
